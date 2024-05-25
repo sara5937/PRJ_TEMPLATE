6e3790cbe56c7ee9d39c4fa62305894b1ccc76f7
unknown <mr.saranraj77@gmail.com>
2024-05-25 09:10:29 +0100

    Prj setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="chatbot",
+    version="0.0.1",
+    author="Saran",
+    author_email="mr.saranraj777@gmail.com",
+    packages=find_packages(),
+    install_requires=["langchain"]
+)
